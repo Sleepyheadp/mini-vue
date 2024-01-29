dc4c4ce959548e9698479cb2a17999bdec6b5b23
sleepyheadp <17863965581@163.com>
2024-01-29 21:33:24 +0800

    mini-vue的雏形

--- index.js
+++ index.js
@@ -27,12 +27,38 @@ v2有个问题就是必须每次都需要手动调用update函数进行更新,
 // v3 reactivity
 // npm init -y
 // 安装一下@vue/reactivity
-let a = reactive({
-	val: 10,
-});
-let b;
-effectWatch(() => {
-	b = a.val;
-	console.log(b);
-});
-a.val = 20;
+// let a = reactive({
+// 	val: 10,
+// });
+// let b;
+// effectWatch(() => {
+// 	b = a.val;
+// 	console.log(b);
+// });
+// a.val = 20;
+
+// 结合UI视图更新
+
+const App = {
+	render() {
+		effectWatch(() => {
+			document.querySelector("#app").innerHTML = ""; // 每次都清空
+			const dom = document.createElement("div");
+			const textOne = document.createTextNode("num:");
+			const textTwo = document.createTextNode(obj.num); // 根据num的值变动
+			dom.append(textOne);
+			dom.append(textTwo);
+			document.querySelector("#app").append(dom);
+		});
+	},
+	setup() {
+		let obj = reactive({
+			num: 0,
+		});
+		window.obj = obj; // 通过obj.num++修改值,而不是window.obj.num++
+		return {
+			obj,
+		};
+	},
+};
+App.render(App.setup());
