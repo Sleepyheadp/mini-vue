ac51efd7dccc0df57c9861f97a128cc9cdd413a8
sleepyheadp <17863965581@163.com>
2024-02-02 17:35:47 +0800

    test lazy:功能二
    这是同页面的功能二

--- lazygit_test.js
+++ lazygit_test.js
@@ -1,3 +1,11 @@
+// change one
+Array.prototype.reduce = function (callback, initialValue) {
+	let accumulator = initialValue !== undefined ? initialValue : this[0];
+	for (let i = initialValue !== undefined ? 0 : 1; i < this.length; i++) {
+		accumulator = callback(accumulator, this[i], i, this);
+	}
+	return accumulator;
+};
 // change two
 Array.prototype.unique = function () {
 	const seen = new Set();
