92de88128ee293893a10859e33a5d77bd28b539e
sleepyheadp <17863965581@163.com>
2024-02-16 13:56:12 +0800

    add file : lazygit_test.js

--- lazygit_test.js
+++ lazygit_test.js
@@ -1,21 +1,11 @@
-// change one
-Array.prototype.reduce = function (callback, initialValue) {
-	let accumulator = initialValue !== undefined ? initialValue : this[0];
-	for (let i = initialValue !== undefined ? 0 : 1; i < this.length; i++) {
-		accumulator = callback(accumulator, this[i], i, this);
+this.name = "capoo";
+add();
+
+function add() {
+	// Implement
+	const count = 2;
+	count;
+	function countaa() {
+		// Implement
 	}
-	return accumulator;
-};
-// change two
-Array.prototype.unique = function () {
-	const seen = new Set();
-	const result = [];
-	for (let i = 0; i < this.length; i++) {
-		const item = this[i];
-		if (!seen.has(item)) {
-			seen.add(item);
-			result.push(item);
-		}
-	}
-	return result;
-};
+}
